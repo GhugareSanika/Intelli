4c89f4e4e16696829f1f601d720bd42a96988d2c
GhugareSanika <144719999+GhugareSanika@users.noreply.github.com>
2024-08-03 00:48:44 +0530

    Initial Commit

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const DashboardPage = () => {
+  return (
+    <Button variant="destructive" size="lg">Click Me</Button>
+)
+}
+
+export default DashboardPage;
